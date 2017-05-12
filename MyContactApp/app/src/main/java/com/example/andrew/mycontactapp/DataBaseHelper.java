0d6a6d7d9566a336323f4da9fde568a510abeb59
Shamusa <nguyena8047@gmail.com>
2017-05-12 08:08:20 -0700

    DataBaseHelper

--- MyContactApp/app/src/main/java/com/example/andrew/mycontactapp/DataBaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/andrew/mycontactapp/DataBaseHelper.java
@@ -9,16 +9,20 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DataBaseHelper extends SQLiteOpenHelper{
+
     public static final String DATABASE_NAME="Contacts.db";
     public static final String TABLE_NAME="contact_table";
     public static final String COL_1="ID";
     public static final String COL_2="NAME";
+
     public DataBaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, null, 1);
     }
+
     public void onCreate(SQLiteDatabase db){
 
     }
+
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
 
     }
