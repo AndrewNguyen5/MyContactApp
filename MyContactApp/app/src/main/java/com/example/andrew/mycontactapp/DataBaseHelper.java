f8c5c63a60035e93bedd704902d9664b7cdb4967
Shamusa <nguyena8047@gmail.com>
2017-05-12 09:26:02 -0700

    5/12

--- MyContactApp/app/src/main/java/com/example/andrew/mycontactapp/DataBaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/andrew/mycontactapp/DataBaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.andrew.mycontactapp;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -15,15 +16,18 @@ public class DataBaseHelper extends SQLiteOpenHelper{
     public static final String COL_1="ID";
     public static final String COL_2="NAME";
 
-    public DataBaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public DataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
 
+    @Override
     public void onCreate(SQLiteDatabase db){
-
+        db.execSQL("CREATE TABLE"+ TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
     }
 
+    @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-
+        db.execSQL("DROP TABLE IF EXISTS"+ TABLE_NAME);
+        onCreate(db);
     }
 }
--- MyContactApp/app/src/main/java/com/example/andrew/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/andrew/mycontactapp/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.andrew.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    DataBaseHelper myDb;
+    EditText editName;
+    Button btAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb=new DataBaseHelper(this);
     }
+
 }
