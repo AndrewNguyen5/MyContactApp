6b291144554f6d4dc323fa493f9125dd3ea2f96b
Shamusa <nguyena8047@gmail.com>
2017-05-15 09:28:19 -0700

    5/15

--- MyContactApp/app/src/main/java/com/example/andrew/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/andrew/mycontactapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.andrew.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb=new DataBaseHelper(this);
+        editName= (EditText) findViewById(R.id.editText_name);
+
+    }
+    public void addData(View v){
+        boolean  isInserted = myDb.insertData(editName.getText().toString());
+
+        if (isInserted==true){
+            Log.d("MyContact","Data insertion successful");
+        }
+        if(isInserted==false){
+            Log.d("MyContact","Data insertion not sucessful");
+        }
     }
 
 }
